b0a4826aa5dbe5972dd2cb2f59f40b24d45472fe
Brian Niedzialkoski <brianniedzialkoski@gmail.com>
2018-07-10 14:45:30 -0400

    Refactor

--- app/src/main/java/com/popularpenguin/retrofit/MainActivity.kt
+++ app/src/main/java/com/popularpenguin/retrofit/MainActivity.kt
@@ -2,47 +2,30 @@ package com.popularpenguin.retrofit
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.google.gson.GsonBuilder
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        viewModel = ViewModel()
+
         tv.text = "Initializing..."
-
-        val udacityUrl = "https://go.udacity.com/"
-
-        val gson = GsonBuilder()
-                .setLenient()
-                .create()
-
-        val retrofit = Retrofit.Builder()
-                .baseUrl(udacityUrl)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-
-        val articleAPI = retrofit.create(ArticleService::class.java)
-        articleAPI.articleList()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe { displayList(it) }
     }
 
-    private fun formatText(article: Article): String {
-        return "${article.id}: ${article.title} by ${article.author}: ${article.body.length}\n\n"
+    override fun onResume() {
+        super.onResume()
+
+        viewModel.subscribe(tv)
     }
 
-    private fun displayList(list: List<Article>) {
-        tv.text = ""
-        list.forEach { tv.append(formatText(it)) }
+    override fun onDestroy() {
+        viewModel.onDestroy()
+
+        super.onDestroy()
     }
 }
---
+++ app/src/main/java/com/popularpenguin/retrofit/ViewModel.kt
@@ -0,0 +1,56 @@
+package com.popularpenguin.retrofit
+
+import android.widget.TextView
+import com.google.gson.GsonBuilder
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ViewModel {
+
+    companion object {
+        const val BASE_URL = "https://go.udacity.com/"
+    }
+
+    private val disposables = CompositeDisposable()
+
+    private val networkObservable: Observable<List<Article>>
+
+    init {
+        val gson = GsonBuilder()
+                .setLenient()
+                .create()
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+
+        val articleAPI = retrofit.create(ArticleService::class.java)
+        networkObservable = articleAPI.articleList()
+    }
+
+    fun subscribe(view: TextView) {
+        fun formatText(article: Article): String {
+            return "${article.id}: ${article.title} by ${article.author}: ${article.body.length}\n\n"
+        }
+
+        val disposable = networkObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { list ->
+                    view.text = ""
+                    list.forEach { view.append(formatText(it)) }
+                }
+
+        disposables.add(disposable)
+    }
+
+    fun onDestroy() {
+        disposables.clear()
+    }
+}
